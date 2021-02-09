7f7e4028977ea8218b12e6fac7c9cb79c0281f81
Inryu <dellintel17@naver.com>
2021-02-09 20:02:13 +0900

    install scan library

--- Screen/HomeStackScreens/MarkScreen.js
+++ Screen/HomeStackScreens/MarkScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, createRef} from 'react';
+import React, {Component, useState, useEffect, createRef, useRef} from 'react';
 
 import {
   widthPercentageToDP as wp,
@@ -7,16 +7,80 @@ import {
 
 import 'react-native-gesture-handler';
 import Loader from '../Components/Loader';
-import {StyleSheet, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import AsyncStorage from '@react-native-community/async-storage';
+import PDFScanner from '@woonivers/react-native-document-scanner';
+import Permissions from 'react-native-permissions';
 
 const MarkScreen = ({route, navigation}) => {
+  const pdfScannerElement = useRef(null);
+  const [data, setData] = useState({});
+  const [allowed, setAllowed] = useState(false);
   const {currentBooksn} = route.params;
+
+  useEffect(() => {
+    async function requestCamera() {
+      const result = await Permissions.request(
+        Platform.OS === 'android'
+          ? 'android.permission.CAMERA'
+          : 'ios.permission.CAMERA',
+      );
+      if (result === 'granted') {
+        setAllowed(true);
+      }
+    }
+    requestCamera();
+  }, []);
+
+  function handleOnPressRetry() {
+    setData({});
+  }
+  function handleOnPress() {
+    pdfScannerElement.current.capture();
+  }
+  if (!allowed) {
+    console.log('You must accept camera permission');
+    return (
+      <View style={styles.permissions}>
+        <Text>You must accept camera permission</Text>
+      </View>
+    );
+  }
+  if (data.croppedImage) {
+    console.log('data', data);
+    return (
+      <React.Fragment>
+        <Image source={{uri: data.croppedImage}} style={styles.preview} />
+        <TouchableOpacity onPress={handleOnPressRetry} style={styles.button}>
+          <Text style={styles.buttonText}>Retry</Text>
+        </TouchableOpacity>
+      </React.Fragment>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>this is MarkScreen</Text>
-    </View>
+    <React.Fragment>
+      <PDFScanner
+        ref={pdfScannerElement}
+        style={styles.scanner}
+        onPictureTaken={setData}
+        overlayColor="rgba(255,130,0, 0.7)"
+        enableTorch={false}
+        quality={0.5}
+        detectionCountBeforeCapture={5}
+        detectionRefreshRateInMS={50}
+      />
+      <TouchableOpacity onPress={handleOnPress} style={styles.button}>
+        <Text style={styles.buttonText}>Take picture</Text>
+      </TouchableOpacity>
+    </React.Fragment>
   );
 };
 
@@ -50,6 +114,29 @@ const styles = StyleSheet.create({
     fontSize: wp('4%'),
     color: 'red',
     paddingTop: wp(2),
+  },
+  scanner: {
+    flex: 1,
+    aspectRatio: undefined,
+  },
+  button: {
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 32,
+  },
+  buttonText: {
+    backgroundColor: 'rgba(245, 252, 255, 0.7)',
+    fontSize: 32,
+  },
+  preview: {
+    flex: 1,
+    width: '100%',
+    resizeMode: 'cover',
+  },
+  permissions: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
